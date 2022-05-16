8d52e1a5d6e22007bb6c040167289c1d1d10b6b5
IvanPKostadinov <ikostadinov@melontech.com>
2022-05-16 18:06:18 +0300

    added React Components to the HomePage and some styling for them

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
-function HomePage() {};
+import { Fragment } from 'react';
+
+function HomePage() {
+  // Typically Page Components are focused on getting Data, not on
+  // styling or HTML. We use React Components for these.
+  return (
+    <Fragment>
+      <Hero />
+      <FeaturedPosts />
+    </Fragment>
+  );
+}
 
 export default HomePage;
+
+// 1) Hero Section -> Present ourselves
+// 2) Featured Posts Section
