e706fd4c5a3ced3fdf23056f2d38b7d2b3afcf09
Ivan.Kostadinov <ivanpkostadinov@gmail.com>
2022-05-18 19:56:06 +0300

    added a PostItem Component to show in PostsGrid

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,3 +1,43 @@
-function PostItem() {}
+import Link from 'next/link';
+import Image from 'next/image';
+
+import classes from './post-item.module.css';
+
+function PostItem(props) {
+  const { title, image, excerpt, date, slug } = props.post;
+
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long', // 01 -> January
+    year: 'numeric',
+  });
+
+  const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
+
+  return (
+    <li className={classes.post}>
+      <Link href={linkPath}>
+        <a>
+          <div className={classes.image}>
+            {/* layout='responsive' will make the image responsive to container/display sizes */}
+            <Image
+              src={imagePath}
+              alt={title}
+              width={300}
+              height={200}
+              layout='responsive'
+            />
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <time>{formattedDate}</time>
+            <p>{excerpt}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
+}
 
 export default PostItem;
