c2c399f345a3f81f27d00d44402413c29bc53e8c
Ivan.Kostadinov <ivanpkostadinov@gmail.com>
2022-05-18 23:29:20 +0300

    added AllPosts Component to render on the /posts Page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,42 @@
-function AllPostsPage() {};
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-startet-with-nextjs',
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js is the React Framework for Production - it helps build fullstack applications.',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-startet-with-nextjs2',
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js is the React Framework for Production - it helps build fullstack applications.',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-startet-with-nextjs3',
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js is the React Framework for Production - it helps build fullstack applications.',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-startet-with-nextjs4',
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js is the React Framework for Production - it helps build fullstack applications.',
+    date: '2022-02-10',
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
