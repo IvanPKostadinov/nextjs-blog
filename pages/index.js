a28769474e03d2bf2a65bbfa1f9caa9f2afc69e8
Ivan.Kostadinov <ivanpkostadinov@gmail.com>
2022-05-18 19:56:34 +0300

    added some dummy data for Posts and some images

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,42 @@
 import { Fragment } from 'react';
 
 import Hero from '../components/home-page/hero';
+import FeaturedPosts from '../components/home-page/featured-posts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-startet-with-nextjs',
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js is the React Framework for Production - it helps build fullstack applications.',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-startet-with-nextjs2',
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js is the React Framework for Production - it helps build fullstack applications.',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-startet-with-nextjs3',
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js is the React Framework for Production - it helps build fullstack applications.',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-startet-with-nextjs4',
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js is the React Framework for Production - it helps build fullstack applications.',
+    date: '2022-02-10',
+  },
+];
 
 function HomePage() {
   // Typically Page Components are focused on getting Data, not on
@@ -8,6 +44,7 @@ function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
